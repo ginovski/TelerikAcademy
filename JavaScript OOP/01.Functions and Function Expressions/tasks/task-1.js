67e3ddddabc6d3fd062eb85ca467480591681fa3
ginovski <sashko.ginovski@gmail.com>
2015-06-17 17:53:41 +0300

    Add JavaScript OOP Function Expressions with tests and README

---
+++ JavaScript OOP/01.Functions and Function Expressions/tasks/task-1.js
@@ -0,0 +1,26 @@
+//1.Write a function that sums an array of numbers:
+//    Numbers must be always of type Number
+//Returns null if the array is empty
+//Throws Error if the parameter is not passed (undefined)
+//Throws if any of the elements is not convertible to Number
+"use strict";
+
+function solve(arr){
+	if(arr.length === undefined){
+		throw new Error();
+	}
+	if(arr.length === 0){
+		return null;
+	}
+	return arr.reduce(function(initial, number){
+		number = +number;
+		if(isNaN(number)){
+			throw new Error();
+		}
+
+		return initial + number;
+	}, 0);
+}
+
+module.exports = solve;
+
