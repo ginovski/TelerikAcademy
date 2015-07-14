e1bbaccc171e4c2f54a64280416ab5a51082c6b2
ginovski <sashko.ginovski@gmail.com>
2015-07-14 22:05:48 +0300

    Added High-Quality Code/4.Variables, Data, Expressions and Constants homework task 1

---
+++ High-Quality Code/4.Variables, Data, Expressions and Constants/VariablesDataExpressionsAndConstants/SizeProject/Size.cs
@@ -0,0 +1,28 @@
+﻿// Refactor the following code to use proper variable naming and simplified expressions:
+namespace SizeProject
+{
+    using System;
+
+    public class Size
+    {
+        private double width;
+        private double height;
+
+        public Size(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public static Size GetRotatedSize(Size size, double figureAngle)
+        {
+            double figureAngleCos = Math.Abs(Math.Cos(figureAngle));
+            double figureAngleSin = Math.Abs(Math.Sin(figureAngle));
+
+            double width = (figureAngleCos * size.width) + (figureAngleSin * size.height);
+            double height = (figureAngleSin * size.width) + (figureAngleCos * size.height);
+
+            return new Size(width, height);
+        }
+    }
+}
