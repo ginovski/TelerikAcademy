24e1219f3afd9e6a3658f7536936f94b442dd80e
ginovski <sashko.ginovski@gmail.com>
2015-07-14 22:14:44 +0300

    Added full homework for Variables and Constants

---
+++ High-Quality Code/4.Variables, Data, Expressions and Constants/VariablesDataExpressionsAndConstants/Statistics/Printer.cs
@@ -0,0 +1,46 @@
+﻿// Refactor the following code to apply variable usage and naming best practices
+namespace Statistics
+{
+    using System;
+
+    public class Printer
+    {
+        public void PrintStatistics(double[] arr, int count)
+        {
+            double max = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            this.Print(max);
+
+            double min = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+
+            this.Print(min);
+
+            double sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sum += arr[i];
+            }
+
+            this.Print(sum / count);
+        }
+
+        private void Print(double value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
