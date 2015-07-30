4e506a661e95f386ef2a6777a09a4941cc7eb0d8
ginovski <sashko.ginovski@gmail.com>
2015-07-30 21:25:19 +0300

    Added homework for High-Quality Code/Exceptions

---
+++ High-Quality Code/8.Defensive Programming and Exceptions/DefensiveProgrammingAndExceptions/Exceptions/ExceptionsHomework.cs
@@ -0,0 +1,62 @@
+﻿namespace Exceptions
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Utils;
+
+    public class ExceptionsHomework
+    {
+        private static void Main()
+        {
+            var substr = StudentSystemUtils.Subsequence("Hello!".ToCharArray(), 2, 3);
+            Console.WriteLine(substr);
+
+            var subarr = StudentSystemUtils.Subsequence(new int[] { -1, 3, 2, 1 }, 0, 2);
+            Console.WriteLine(String.Join(" ", subarr));
+
+            var allarr = StudentSystemUtils.Subsequence(new int[] { -1, 3, 2, 1 }, 0, 4);
+            Console.WriteLine(String.Join(" ", allarr));
+
+            var emptyarr = StudentSystemUtils.Subsequence(new int[] { -1, 3, 2, 1 }, 0, 0);
+            Console.WriteLine(String.Join(" ", emptyarr));
+
+            Console.WriteLine(StudentSystemUtils.ExtractEnding("I love C#", 2));
+            Console.WriteLine(StudentSystemUtils.ExtractEnding("Nakov", 4));
+            Console.WriteLine(StudentSystemUtils.ExtractEnding("beer", 4));
+            // Throws error because count is bigger than string length
+            // Console.WriteLine(StudentSystemUtils.ExtractEnding("Hi", 100));
+
+            if (StudentSystemUtils.CheckPrime(23))
+            {
+                Console.WriteLine("23 is prime.");
+            }
+            else
+            {
+                Console.WriteLine("23 is not prime");
+            }
+
+            if (StudentSystemUtils.CheckPrime(33))
+            {
+                Console.WriteLine("33 is prime.");
+            }
+            else
+            {
+                Console.WriteLine("33 is not prime");
+            }
+
+            List<Exam> peterExams = new List<Exam>()
+            {
+                new SimpleMathExam(2),
+                new CSharpExam(55),
+                new CSharpExam(100),
+                new SimpleMathExam(1),
+                new CSharpExam(0),
+            };
+
+            Student peter = new Student("Peter", "Petrov", peterExams);
+            double peterAverageResult = peter.CalcAverageExamResultInPercents();
+            Console.WriteLine("Average results = {0:p0}", peterAverageResult);
+        }
+    }
+}
---
+++ High-Quality Code/8.Defensive Programming and Exceptions/DefensiveProgrammingAndExceptions/Exceptions/Utils/StudentSystemUtils.cs
@@ -0,0 +1,74 @@
+﻿namespace Exceptions.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StudentSystemUtils
+    {
+        public static T[] Subsequence<T>(T[] arr, int startIndex, int count)
+        {
+            if (arr == null || arr.Length == 0)
+            {
+                throw new ArgumentNullException("Array can not be null or empty");
+            }
+
+            if (startIndex < 0 || startIndex >= arr.Length)
+            {
+                throw new ArgumentOutOfRangeException("Start index is out of range");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException("Count must be positive");
+            }
+
+            List<T> result = new List<T>();
+            for (int i = startIndex; i < startIndex + count; i++)
+            {
+                result.Add(arr[i]);
+            }
+
+            return result.ToArray();
+        }
+
+        public static string ExtractEnding(string str, int count)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentNullException("String can not be null or empty");
+            }
+
+            if (count > str.Length)
+            {
+                throw new ArgumentOutOfRangeException("Count is out of string range");
+            }
+
+            StringBuilder result = new StringBuilder();
+            for (int i = str.Length - count; i < str.Length; i++)
+            {
+                result.Append(str[i]);
+            }
+
+            return result.ToString();
+        }
+
+        public static bool CheckPrime(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException("Number must be positive");
+            }
+
+            for (int divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+            {
+                if (number % divisor == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
