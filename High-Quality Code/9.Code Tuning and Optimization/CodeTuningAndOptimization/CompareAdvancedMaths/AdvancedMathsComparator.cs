089d2b5e22876240933e3c43871cee40bfb3f4df
ginovski <sashko.ginovski@gmail.com>
2015-07-31 21:12:38 +0300

    Added full homework for High-Quality Code/Code Optimization

---
+++ High-Quality Code/9.Code Tuning and Optimization/CodeTuningAndOptimization/CompareAdvancedMaths/AdvancedMathsComparator.cs
@@ -0,0 +1,105 @@
+﻿namespace CompareAdvancedMaths
+{
+    using System;
+    using System.Diagnostics;
+
+    public class AdvancedMathsComparator
+    {
+        private Stopwatch sw;
+
+        public AdvancedMathsComparator()
+        {
+            this.sw = new Stopwatch();
+        }
+
+        public TimeSpan CompareSquareRoot(float number)
+        {
+            this.sw.Reset();
+            this.sw.Start();
+
+            float result = (float)Math.Sqrt(number);
+
+            return this.sw.Elapsed;
+        }
+
+        public TimeSpan CompareSquareRoot(double number)
+        {
+            this.sw.Reset();
+            this.sw.Start();
+
+            double result = Math.Sqrt(number);
+
+            return this.sw.Elapsed;
+        }
+
+        public TimeSpan CompareSquareRoot(decimal number)
+        {
+            this.sw.Reset();
+            this.sw.Start();
+
+            decimal result = (decimal)Math.Sqrt((double)number);
+
+            return this.sw.Elapsed;
+        }
+
+        public TimeSpan CompareLogarithm(float number)
+        {
+            this.sw.Reset();
+            this.sw.Start();
+
+            float result = (float)Math.Log(number);
+
+            return this.sw.Elapsed;
+        }
+
+        public TimeSpan CompareLogarithm(double number)
+        {
+            this.sw.Reset();
+            this.sw.Start();
+
+            double result = Math.Log(number);
+
+            return this.sw.Elapsed;
+        }
+
+        public TimeSpan CompareLogarithm(decimal number)
+        {
+            this.sw.Reset();
+            this.sw.Start();
+
+            decimal result = (decimal)Math.Log((double)number);
+
+            return this.sw.Elapsed;
+        }
+
+        public TimeSpan CompareSinus(float number)
+        {
+            this.sw.Reset();
+            this.sw.Start();
+
+            float result = (float)Math.Sin(number);
+
+            return this.sw.Elapsed;
+        }
+
+        public TimeSpan CompareSinus(double number)
+        {
+            this.sw.Reset();
+            this.sw.Start();
+
+            double result = Math.Sin(number);
+
+            return this.sw.Elapsed;
+        }
+
+        public TimeSpan CompareSinus(decimal number)
+        {
+            this.sw.Reset();
+            this.sw.Start();
+
+            decimal result = (decimal)Math.Sin((double)number);
+
+            return this.sw.Elapsed;
+        }
+    }
+}
---
+++ High-Quality Code/9.Code Tuning and Optimization/CodeTuningAndOptimization/CompareAdvancedMaths/Engine.cs
@@ -0,0 +1,32 @@
+﻿namespace CompareAdvancedMaths
+{
+    using System;
+
+    public class Engine
+    {
+        private static void Main(string[] args)
+        {
+            var comparator = new AdvancedMathsComparator();
+
+            float numberFloat = 6.88888f;
+            double numberDouble = 8.43412323;
+            decimal numberDecimal = 10.324233242m;
+
+            Console.WriteLine("Float square root: {0}", comparator.CompareSquareRoot(numberFloat));
+            Console.WriteLine("Double square root: {0}", comparator.CompareSquareRoot(numberDouble));
+            Console.WriteLine("Decimal square root: {0}", comparator.CompareSquareRoot(numberDecimal));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Float logarithm: {0}", comparator.CompareLogarithm(numberFloat));
+            Console.WriteLine("Double logarithm: {0}", comparator.CompareLogarithm(numberDouble));
+            Console.WriteLine("Decimal logarithm: {0}", comparator.CompareLogarithm(numberDecimal));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Float sinus: {0}", comparator.CompareSinus(numberFloat));
+            Console.WriteLine("Double sinus: {0}", comparator.CompareSinus(numberDouble));
+            Console.WriteLine("Decimal sinus: {0}", comparator.CompareSinus(numberDecimal));
+        }
+    }
+}
