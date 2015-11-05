426c1dbb913e6c5a25e9784bea407eaf95b018ee
ginovski <sashko.ginovski@gmail.com>
2015-11-05 19:26:32 +0200

    Added workshop for data structures

---
+++ Data Structures and Algorithms/05. Data Structures Workshop/JediMeditation/JediMeditation/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace JediMeditation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            var jedis = Console.ReadLine().Split(' ');
+
+            var jedisDivided = new Dictionary<string, Queue<string>>();
+
+            jedisDivided["m"] = new Queue<string>();
+            jedisDivided["k"] = new Queue<string>();
+            jedisDivided["p"] = new Queue<string>();
+
+            foreach (var jedi in jedis)
+            {
+                if (jedi.StartsWith("m"))
+                {
+                    jedisDivided["m"].Enqueue(jedi);
+                }
+                else if (jedi.StartsWith("k"))
+                {
+                    jedisDivided["k"].Enqueue(jedi);
+                }
+                else
+                {
+                    jedisDivided["p"].Enqueue(jedi);
+                }
+            }
+
+            var result = new StringBuilder();
+
+            result.Append(string.Join(" ", jedisDivided["m"]) + " ");
+            result.Append(string.Join(" ", jedisDivided["k"]) + " ");
+            result.Append(string.Join(" ", jedisDivided["p"]));
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
