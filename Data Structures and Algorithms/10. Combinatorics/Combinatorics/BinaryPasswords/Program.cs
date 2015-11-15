d4aaaf125b0177e8412d59dae676fc482d75cd91
ginovski <sashko.ginovski@gmail.com>
2015-11-15 21:52:17 +0200

    Added some combinatorics homework and workshop

---
+++ Data Structures and Algorithms/10. Combinatorics/Combinatorics/BinaryPasswords/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace BinaryPasswords
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            long finalResult = 1;
+            string pattern = Console.ReadLine();
+            int[] missingPatterns = new int[pattern.Length];
+            int missingPatternIndex = 0;
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                char sym = pattern[i];
+                if (sym == '*')
+                {
+                    missingPatterns[missingPatternIndex]++;
+                }
+                else if (sym == '1' || sym == '0')
+                {
+                    if (i != 0 && pattern[i - 1] == '*')
+                    {
+                        missingPatternIndex++;
+                    }
+                }
+            }
+
+            if (missingPatterns[0] == 0)
+            {
+                Console.WriteLine(1);
+                return;
+            }
+
+            foreach (var missingPatternLength in missingPatterns)
+            {
+                if (missingPatternLength == 0)
+                {
+                    break;
+                }
+
+                long result = 0;
+
+                result += CalcVariationsCount(missingPatternLength);
+
+                finalResult *= result;
+            }
+
+            Console.WriteLine(finalResult);
+        }
+
+        private static long CalcVariationsCount(int k)
+        {
+            long result = 1;
+
+            for (int i = 0; i < k; i++)
+            {
+                result *= 2;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Data Structures and Algorithms/12. Combinatorics Workshop/CombinatoricsWorkshop/BinaryTrees/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees
+{
+    internal class Program
+    {
+        private static BigInteger[] factorials;
+
+        private static void Main(string[] args)
+        {
+            string buckets = Console.ReadLine();
+            int[] groups = new int[26];
+
+            foreach (var bucket in buckets)
+            {
+                groups[bucket - 'A']++;
+            }
+
+            int n = buckets.Length;
+
+            factorials = new BigInteger[2 * n + 1];
+            factorials[0] = 1;
+
+            for (int i = 1; i < factorials.Length; i++)
+            {
+                factorials[i] = factorials[i - 1] * i;
+            }
+
+            BigInteger result = factorials[n];
+            foreach (var group in groups)
+            {
+                result /= factorials[group];
+            }
+
+            BigInteger numberOfTrees = GetNumberOfTrees(n);
+
+            Console.WriteLine(result * numberOfTrees);
+        }
+
+        private static BigInteger GetNumberOfTrees(int n)
+        {
+            return factorials[2 * n] / (factorials[n + 1] * factorials[n]);
+        }
+    }
+}
